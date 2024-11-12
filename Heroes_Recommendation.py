8a372cf9b07cb8b0ac682298464d5eaddac7afe9
fredyico <118931952+fredyico@users.noreply.github.com>
2024-11-11 20:18:56 -0400

    1

---
+++ Heroes_Recommendation.py
@@ -0,0 +1,16 @@
+
+
+def greet():
+  print("Hi there and welcome to Dota2 Recommendation System!")
+  print("We'll help you find the best and suited hero for you.")
+
+def goodbye():
+  print("Thanks for using D2RS. See ya later.")
+
+def choose_hero():
+    pass
+
+def skyroute():
+  greet()
+  choose_hero()
+  goodbye()
