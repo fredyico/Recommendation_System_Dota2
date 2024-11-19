181ee751f32528e59e4c19026a7d126d787b4c5a
fredyico <118931952+fredyico@users.noreply.github.com>
2024-11-19 18:44:01 -0400

    Update Heroes_Recommendation.py

--- Heroes_Recommendation.py
+++ Heroes_Recommendation.py
@@ -3,19 +3,16 @@
 def greet():
   print("Hi there and welcome to Dota2 Recommendation System!")
   print("We'll help you find the best and suited hero for you.")
-  print("What would you like to do?\n 1.Hero based on position/role? \n 2.Counter-Pick suggestion? \n ")
+  print("What would you like to do?\n 1. Recommendation heroes by first letter? 2. Recommendation heroes by characteristics?")
 
 def goodbye():
   print("Thanks for using D2RS. See you next time.")
 
 def choose_hero():
-    pass
-
-def hero_position():
-    hero_pos = input("Choose the hero position that you would like. 1)Pos1(midlaner)\n 2)Pos2(safelaner/carry)\n 3)Pos3(offlaner)\n 4)Pos4(soft support)\n 5)Pos5(hard support)\n")
-
+  pass
+    
 def counter_pick_hero():    
-    counter_pick = input("What is the hero that you would like to counter?\n")
+  counter_pick = input("What is the hero that you would like to counter?\n")
 
 def Dota2_Recomm_System():
   greet()
